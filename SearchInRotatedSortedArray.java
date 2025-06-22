649cc4bd8d92f95ffc68b0b2f3c6b55e0992ce44
Ishan Tharwal <ishan.tharwal@educationatwork.org>
2025-06-22 03:33:23 -0700

    Done BinarySearch1

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,11 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Had to revise basic syntax
+
+class Solution {
+    public int search(int[] nums, int target) {
+
+        
+    }
+}
