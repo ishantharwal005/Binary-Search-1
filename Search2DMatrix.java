2c9f8524adf39cbc1b1dd71159f0964db8a9f580
Ishan Tharwal <ishan.tharwal@educationatwork.org>
2025-06-22 04:47:37 -0700

    Done BinarySearch1

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -1,7 +1,7 @@
 // Time Complexity : O(log(m*n))
 // Space Complexity : O(1)
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Had to revise basic syntax
 
 
 class Solution {
--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -5,7 +5,37 @@
 
 class Solution {
     public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
 
-        
+        while(low <= high){
+            int mid = low + (high - low) / 2; // To prevent Integer Overflow
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(target < nums[mid] && target >= nums[low]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+            
+        }
+        return -1;
+
     }
 }
